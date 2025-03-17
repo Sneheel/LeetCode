3dbf5d710c4cb916e11f44bf406996d967c96b90
Sneheel Shivam <shivamsneheel@gmail.com>
2025-03-18 01:39:55 +0530

    added optimal solution

--- src/Easy/Merge_Sorted_Array_88.java
+++ src/Easy/Merge_Sorted_Array_88.java
@@ -35,3 +35,21 @@ public class Merge_Sorted_Array_88 {
         }
     }
 }
+
+/* Optimal Solution - iterate from the end
+public void merge(int[] nums1, int m, int[] nums2, int n) {
+    int midx = m - 1;
+    int nidx = n - 1;
+    int right = m + n - 1;
+
+    while (nidx >= 0) {
+        if (midx >= 0 && nums1[midx] > nums2[nidx]) {
+            nums1[right] = nums1[midx];
+            midx--;
+        } else {
+            nums1[right] = nums2[nidx];
+            nidx--;
+        }
+        right--;
+    }
+}*/
